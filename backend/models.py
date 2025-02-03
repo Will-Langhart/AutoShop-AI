f72b85b447d51befc535924bc6fb8322472742af
Will Langhart <theholotrout@gmail.com>
2025-02-03 13:34:05 -0500

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+
+class Store(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    owner_email = db.Column(db.String(255), nullable=False, unique=True)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    store_id = db.Column(db.Integer, db.ForeignKey('store.id'), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    image_url = db.Column(db.String(255), nullable=True)
